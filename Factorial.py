c677433f725915eb907c48253590d94bfcb99ae9
Md. Asadur Rahaman <asadurrahaman2005@gmail.com>
2026-01-13 12:10:42 +0530

    Factorial calculation Recursive function in Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+#Fatorial
+def fact(num):
+    if num == 0:
+        return 1
+    else:
+        return num * fact(num - 1)
+
+number = int(input("Enter a number to find factorial: "))
+result = fact(number)
+print("Factorial: ", result)
