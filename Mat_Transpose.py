b689b3bb49b677a43da7f224bc965b94107c3cfc
Md. Asadur Rahaman <asadurrahaman2005@gmail.com>
2026-03-31 12:31:56 +0530

    Update matrix dimensions for transposition

--- Mat_Transpose.py
+++ Mat_Transpose.py
@@ -1,5 +1,5 @@
-mat = [[1, 2, 3], [4, 5, 6]]
-result = [[0, 0], [0, 0], [0, 0]]
+mat = [[1, 2, 3], [4, 5, 6],[7, 8, 9]]
+result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for i in range(len(mat)):
     for j in range(len(mat[0])):
         result[j][i] = mat[i][j]
